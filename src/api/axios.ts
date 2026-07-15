fdd6ca46917876e73756305942317d8c61966567
Dammar bhatt <dammarbhatt111@gmail.com>
2026-07-15 11:03:17 +0545

    base_URL  updated for deployment

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL : import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL : import.meta.env.VITE_API_URL || "https://hotelbookingapi-production-81c6.up.railway.app",
 })
 
 // Attach token to every request automatically
